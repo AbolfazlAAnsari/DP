e4773cfa8cc73d0e04734385211b84a7b81bc7aa
Abolfazl A.Ansari <77450405+AbolfazlAAnsari@users.noreply.github.com>
2025-04-27 15:44:27 -0400

    Update test.py

--- test.py
+++ test.py
@@ -2,12 +2,13 @@ from transformers import AutoTokenizer, AutoModelForSeq2SeqLM
 import torch
 
 # 1. Load tokenizer and model (pre-trained version)
-model_name = 't5-small'
+# model_name = 'google/t5-small'  # or 'google/flan-t5-base
+model_name = 't5-small'  # or 'google/flan-t5-base'
 tokenizer = AutoTokenizer.from_pretrained(model_name)
 pretrained_model = AutoModelForSeq2SeqLM.from_pretrained(model_name)
 
 # 2. Load the fine-tuned model (based on the English dataset)
-fine_tuned_model_path = '/Users/abolfazlansari/Documents/Pike/Code/DP/DP/results-pii-masking-400k-t5-small-full/normal-loss-final'
+fine_tuned_model_path = '/Users/abolfazlansari/Documents/Pike/Code/DP/DP/models/PHI/results-local-t5-small/normal-loss-final'
 fine_tuned_model = AutoModelForSeq2SeqLM.from_pretrained(fine_tuned_model_path)
 
 # 3. Function to make predictions using the model
@@ -21,11 +22,13 @@ def predict(model, sentence):
     return decoded_output
 
 # 4. Test sentence
-test_sentence = """ My name is Abolfazl Ansari and I am a software engineer. My email is test@gmail.com and my phone number is 123-456-7890. I live in Tehran, Iran. I have weekly visit to hospital"""
+test_sentence = """Chen Wei entered a smoking cessation program after his diagnosis of chronic obstructive pulmonary disease."""
+print("#" * 30)
+print("Test sentence:\n", test_sentence)
+print("#" * 30)
 pretrained_prediction = predict(pretrained_model, test_sentence)
-print("Pre-trained model prediction:", pretrained_prediction)
-
+print("Pre-trained model prediction:\n", pretrained_prediction)
+print("#" * 30)
 # 6. Make predictions with the fine-tuned model (trained only on English data)
 fine_tuned_prediction = predict(fine_tuned_model, test_sentence)
-print("Fine-tuned model prediction:", fine_tuned_prediction)
-
+print("Fine-tuned model prediction:\n", fine_tuned_prediction)
